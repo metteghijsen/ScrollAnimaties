13973855fdfd6d518fcc3c8a9cadbcd0bca2121a
I481695 <mette.ghijsen@student.fontys.nl>
2021-11-18 14:28:32 +0100

    Animaties toegevoegd door gebruik van AOSjs en pagina gestyled

--- index.php
+++ index.php
@@ -5,9 +5,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="style.css">
     <title>Scroll Animaties</title>
 </head>
 <body>
+<div class="content">
+    <div class="container-intro" data-aos="fade-up" data-aos-duration="2000" ><p>Mette Ghijsen</p><h1>AOSjs Demo</h1><h1>Scroll naar beneden</h1><h1 style="font-size: 80px">🡻</h1></div>
+    <div data-aos="fade-up" data-aos-duration="2000" class="container"><p>Fade Up</p></div>
+    <div data-aos="fade-down" data-aos-duration="2000" class="container gray"><p>Fade Down</p></div>
+    <div data-aos="fade-right" data-aos-duration="2000" class="container"><p>Fade Right</p></div>
+    <div data-aos="fade-left" data-aos-duration="2000" class="container gray"><p>Fade Left</p></div>
+    <div data-aos="fade-up-right" data-aos-duration="2000" class="container"><p>Fade Up Right</p></div>
+    <div data-aos="fade-up-left" data-aos-duration="2000" class="container gray"><p>Fade Up Left</p></div>
+    <div data-aos="fade-down-right" data-aos-duration="2000" class="container"><p>Fade Down Right</p></div>
+    <div data-aos="fade-down-left" data-aos-duration="2000" class="container gray"><p>Fade Down Left</p></div>
+</div>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
 
 </body>
 </html>
